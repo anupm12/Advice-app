7bb1719b64f5c25f5e760dcd4f3e7cc17253adf4
anupm12 <shuklaanupam18@gmail.com>
2021-02-11 16:46:17 +0530

    Mark favortite

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,9 +1,17 @@
 const SearchComponent = (props) => {
+  const saveAdvice = (id) => {
+    console.log("clicked" + id);
+  };
+
   const searchAdvice = props.searchAdvice;
   return (
     <div>
       {searchAdvice.data.slips ? (
-        searchAdvice.data.slips.map((val) => <p key={val.id}>{val.advice}</p>)
+        searchAdvice.data.slips.map((val) => (
+          <p onClick={() => saveAdvice(val.id)} key={val.id}>
+            {val.advice}
+          </p>
+        ))
       ) : (
         <p>Nada</p>
       )}
